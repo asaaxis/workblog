2ce07941f4157f63596f412da13a7d0f7135117f
akiasa oshima <asaolr4@gmail.com>
2022-10-26 15:39:36 +0900

    ペアワーク

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,48 @@
+class BlogsController < ApplicationController
+  def new
+    @blog = Blog.new
+  end
+  
+  def index
+    @blogs = Blog.all
+  end
+  
+  def create 
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      byebug
+      redirect_to blogs_path, notice: "ブログを作成しました！"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @blog = Blog.find(params[:id])
+  end
+
+  def show
+    @blog = Blog.find(params[:id])
+  end
+
+  def update
+    @blog = Blog.find(params[:id])
+    if @blog.update(blog_params)
+      redirect_to blogs_path, notice: "ブログを編集しました！"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @blog = Blog.find(params[:id])
+    @blog.destroy
+  redirect_to blogs_path, notice:"ブログを削除しました！"
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :content)
+  end
+end
